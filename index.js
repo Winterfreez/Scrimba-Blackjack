3fe223d926c26bf3cb81924115a4348f73ae6638
Winterfreez <117199522+Winterfreez@users.noreply.github.com>
2022-12-07 00:25:32 -0500

    Added some of dealer framework

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-// 2. Create the player object. Give it two keys, name and chips, and set their values
+// set the player object
 let player = {
     name: "Geno",
     chips: 100
 }
+
+// initialize dealer attributes
+let dealerCards = []
+let dealerSum = 0
+let dealerBlackjack = false
+let dealerAlive = false
+
+// initialize player attributes
 let cards = []
 let sum = 0
 let hasBlackJack = false
 let isAlive = false
-let message = ""
-const messageEl = document.getElementById("message-el")
-const sumEl = document.getElementById("sum-el")
-const cardsEl = document.getElementById("cards-el")
-// 3. Grab ahold of the player-el paragraph and store it in a variable called playerEl
-const playerEl = document.getElementById("player-el")
 
-// 4. Render the player's name and chips in playerEl
+// initialize game state message to empty
+let message = ""
+
+// grab the needed elements and store with const
+// player elements
+const messageEl = document.getElementById("message-el")
+const playerSumEl = document.getElementById("player-sum-el")
+const playerCardsEl = document.getElementById("player-cards-el")
+const playerEl = document.getElementById("player-el")
+// dealer elements
+const dealerSumEl = document.getElementById("dealer-sum-el")
+const dealerCardsEl = document.getElementById("dealer-cards-el")
+
+// render player name and chip count
 playerEl.textContent = player.name + " $" + player.chips
 
 function getRandomCard() {
@@ -29,21 +44,34 @@ function getRandomCard() {
 }
 
 function startGame() {
+    // deal player cards
     isAlive = true
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
     cards = [firstCard, secondCard]
     sum = firstCard + secondCard
+    // deal dealer cards
+    dealerAlive = true
+    let dealerHidden = "?"
+    let dealerShown = getRandomCard()
+    dealerCards = [dealerHidden, dealerShown]
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+    // render dealer state
+    dealerCardsEl.textContent = "Cards: "
+    for (let i = 0; i < dealerCards.length; i++) {
+        dealerCardsEl.textContent += dealerCards[i] + " "
     }
-    
-    sumEl.textContent = "Sum: " + sum
+    dealerSumEl.textContent = "Sum: " + sum
+    // render player state
+    playerCardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        playerCardsEl.textContent += cards[i] + " "
+    }
+    playerSumEl.textContent = "Sum: " + sum
+    // check player sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -55,7 +83,6 @@ function renderGame() {
     }
     messageEl.textContent = message
 }
-
 
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
