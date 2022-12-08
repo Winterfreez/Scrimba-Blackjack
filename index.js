7be5f403595a27cac5f430f36068452a0c534404
Winterfreez <angeltechservices@gmail.com>
2022-12-08 13:13:48 -0500

    Developing Dealer Function
    Finished core developer functionality.  Began showing and hiding UI buttons for greater ease of use.  Need to finish applying styling to divs instead of individual buttons.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let isAlive = false
 // initialize game state message to empty
 let message = ""
 
-// grab the needed elements and store with const
+// grab the needed elements
 // player elements
 const messageEl = document.getElementById("message-el")
 const playerSumEl = document.getElementById("player-sum-el")
@@ -28,6 +28,10 @@ const playerEl = document.getElementById("player-el")
 // dealer elements
 const dealerSumEl = document.getElementById("dealer-sum-el")
 const dealerCardsEl = document.getElementById("dealer-cards-el")
+// UI elements
+const dealBtn = document.getElementById("deal-btn")
+const hitBtn = document.getElementById("hit-btn")
+const standBtn = document.getElementById("stand-btn")
 
 // render player name and chip count
 playerEl.textContent = player.name + " $" + player.chips
@@ -44,44 +48,64 @@ function getRandomCard() {
 }
 
 function startGame() {
-    // set player cards
-    isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
+    dealPlayer()
     // set dealer cards
     dealerAlive = true
     let dealerHidden = "?"
     let dealerShown = getRandomCard()
     dealerCards = [dealerHidden, dealerShown]
-    renderGame()
+    dealerSum = 0
+    renderPlayer()
+    renderDealer()
+    hitBtn.style.display = "block"
+    standBtn.style.display = "block"
+    dealBtn.style.display = "none"
 }
 
-function renderGame() {
-    // render dealer state
+function dealPlayer() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+}
+
+
+function renderDealer(){
     dealerCardsEl.textContent = "Cards: "
     for (let i = 0; i < dealerCards.length; i++) {
-        dealerCardsEl.textContent += dealerCards[i] + " "
+        dealerCardsEl.textContent += `${dealerCards[i]} `
     }
-    dealerSumEl.textContent = "Sum: ??"
-    // render player state
+    if (dealerSum === 0) {
+        dealerSumEl.textContent = "Sum: ??"
+    } else if (dealerSum <= 20) {
+        dealerSumEl.textContent = `Sum: ${dealerSum}`
+    } else if (dealerSum === 21) {
+        dealerBlackjack = true
+        dealerSumEl.textContent = `Sum: ${dealerSum}`
+    } else {
+        dealerAlive = false
+        dealerSumEl.textContent = `Sum: ${dealerSum}`
+    }
+}
+
+
+function renderPlayer() {
     playerCardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
         playerCardsEl.textContent += cards[i] + " "
     }
     playerSumEl.textContent = "Sum: " + sum
-    // check player sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
+    message = "Do you want to hit or stand?"
+
+    if (sum === 21) {
         hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
+        dealerPlays()
+    } else if (sum > 21) {
         isAlive = false
+        message = "You busted! The dealer wins"
     }
-    messageEl.textContent = message
+    renderMessage()
 }
 
 function newCard() {
@@ -89,21 +113,40 @@ function newCard() {
         let card = getRandomCard()
         sum += card
         cards.push(card)
-        renderGame()        
+        renderPlayer()        
     }
 }
 
-function dealerPlay() {
-  dealerHidden = getRandomCard()
-  dealerSum = dealerHidden + dealerShown
-  for (dealerAlive === true && dealerBlackjack === false) {
-    dealerSum 
-    if (dealerSum <= 16) {
-      dealerCards.push(getRandomCard())
-    } else if (dealerSum = 21) {
-      dealerBlackjack = true
-    } else if (dealerSum > 21) {
-      dealerAlive = false
+function dealerPlays(){
+    dealerCards.shift()
+    dealerCards.unshift(getRandomCard())
+    for (let i = 0; i < dealerCards.length; i++) {
+        dealerSum += dealerCards[i]
     }
-  }
+    renderDealer()
+}
+
+function stand() {
+    dealerPlays()
+    callWinner()
+    hitBtn.style.display = "none"
+    standBtn.style.display = "none"
+    dealBtn.style.display = "block"
+}
+
+function callWinner() {
+    if (sum === dealerSum) {
+        message = "It's a draw, you get your chips back"
+    } else if (dealerBlackjack === true){
+        message = "Dealer has 21, better luck next time"
+    } else if (sum < dealerSum) {
+        message = `Dealer wins with ${dealerSum}, better luck next time`
+    } else if (sum > dealerSum) {
+        message = `Awesome! You win with ${sum}!`
+    }
+    renderMessage()
+}
+
+function renderMessage() {
+    messageEl.textContent = message
 }
